88107d312d9513ec11426e2a3111a6d3c3f5bbc0
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2023-06-15 00:04:03 +0600

    One to many entity mapping

---
+++ src/main/java/com/ZahidHasanJamil/SBentity/model/Comment.java
@@ -0,0 +1,33 @@
+package com.ZahidHasanJamil.SBentity.model;
+
+//import javax.persistence.*;
+import jakarta.persistence.*; // for Spring Boot 3
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String content;
+
+    // getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
